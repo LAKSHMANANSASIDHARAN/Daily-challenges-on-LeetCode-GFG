ff7e8489b9f3bb7042d3c78f78956382890a1931
S.Lakshmanan <d45624312@gmail.com>
2025-02-28 17:58:02 +0530

    Update day6.cpp

--- day6.cpp
+++ day6.cpp
@@ -1,5 +1,5 @@
 /* Shortest Common Supersequence 
-Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences. If there are multiple valid strings, return any of them.
+Given two strings str1 and str2, return the  shortest string that has both str1 and str2 as subsequences. If there are multiple valid strings, return any of them.
 A string s is a subsequence of string t if deleting some number of characters from t (possibly 0) results in the string s.
 Example 1:
 Input: str1 = "abac", str2 = "cab"

a0e090b562846b270eb5cc1970a7c33cb6f6e8b1
S.Lakshmanan <d45624312@gmail.com>
2025-03-16 21:02:22 +0530

    Update day22.cpp

--- day22.cpp
+++ day22.cpp
@@ -38,3 +38,52 @@ Explanation: First we jump from the 1st to 2nd element and then jump to the last
 Input: arr = [0, 10, 20]
 Output: -1
 Explanation: We cannot go anywhere from the 1st element.
+/*Minimum Time to Repair Cars
+You are given an integer array ranks representing the ranks of some mechanics. ranksi is the rank of the ith mechanic. A mechanic with a rank r can repair n cars in r * n2 minutes.
+You are also given an integer cars representing the total number of cars waiting in the garage to be repaired.
+Return the minimum time taken to repair all the cars.
+Note: All the mechanics can repair the cars simultaneously.
+Example 1:
+Input: ranks = [4,2,3,1], cars = 10
+Output: 16
+Explanation: 
+- The first mechanic will repair two cars. The time required is 4 * 2 * 2 = 16 minutes.
+- The second mechanic will repair two cars. The time required is 2 * 2 * 2 = 8 minutes.
+- The third mechanic will repair two cars. The time required is 3 * 2 * 2 = 12 minutes.
+- The fourth mechanic will repair four cars. The time required is 1 * 4 * 4 = 16 minutes.
+It can be proved that the cars cannot be repaired in less than 16 minutes.​​​​​
+Example 2:
+Input: ranks = [5,1,8], cars = 6
+Output: 16
+Explanation: 
+- The first mechanic will repair one car. The time required is 5 * 1 * 1 = 5 minutes.
+- The second mechanic will repair four cars. The time required is 1 * 4 * 4 = 16 minutes.
+- The third mechanic will repair one car. The time required is 8 * 1 * 1 = 8 minutes.
+It can be proved that the cars cannot be repaired in less than 16 minutes.​​​​​*/
+  class Solution {
+    public:
+    bool process(vector<int>& ranks, int& cars, long long mini){
+        long long cars2 = 0;
+        for (auto r : ranks) {
+            long long c2 = mini / r;
+            long long c = (long long)sqrt(c2);
+            cars2 += c;
+            if (cars2 >= cars) return true; 
+        }
+        return false;
+    }
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long l=1,r=1e14;
+        while(l<r){
+            long long mid=(l+r)/2;
+            if(process(ranks,cars,mid)){
+                r=mid;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+};

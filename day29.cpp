11e96d40fd50f5a8b6083f9ae4337269dcafba51
S.Lakshmanan <d45624312@gmail.com>
2025-04-08 22:36:32 +0530

    Update day29.cpp

--- day29.cpp
+++ day29.cpp
@@ -46,3 +46,42 @@ public:
        return ans;
     }
 };
+/* Excel Sheet Column Title
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+Example 1:
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+Input: columnNumber = 701
+Output: "ZY"*/
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        vector<char> hash(26);
+        int val = 'A';
+        for (int i = 0; i < 26; i++) {
+            hash[i] = val + i;
+        }
+        string ans;
+        while (columnNumber > 0) {
+            columnNumber--; 
+            int temp = columnNumber % 26;
+            ans += hash[temp];
+            columnNumber /= 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

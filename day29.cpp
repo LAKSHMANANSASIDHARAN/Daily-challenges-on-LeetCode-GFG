4e6b6195c3528524c397eab02547c402ba85e191
S.Lakshmanan <d45624312@gmail.com>
2025-04-08 22:38:20 +0530

    Update day29.cpp

--- day29.cpp
+++ day29.cpp
@@ -85,3 +85,50 @@ public:
         return ans;
     }
 };
+/*Longest Palindrome
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+Example 2:
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.*/
+class Solution {
+   static bool compare(const pair<char, int>& a, const pair<char, int>& b) {
+    return a.second > b.second;
+}
+
+public:
+    int longestPalindrome(string s) {
+        vector<pair<char, int>> mp(52, {'0', 0});
+      for (char it : s) {
+        int idx;
+        if (islower(it)) idx = it - 'a';
+        else idx = it - 'A' + 26;
+
+        if (mp[idx].second != 0) {
+            mp[idx].second++;
+        } else {
+            mp[idx] = {it, 1};
+        }
+    }
+
+    sort(mp.begin(), mp.end(), compare);
+    int ans = 0;
+    bool oddUsed = false;
+for (auto it : mp) {
+    if (it.second % 2 == 0) {
+        ans += it.second;
+    } else {
+        ans += it.second - 1;
+        oddUsed = true;
+    }
+}
+if (oddUsed) ans += 1;
+    return ans;
+
+    }
+};

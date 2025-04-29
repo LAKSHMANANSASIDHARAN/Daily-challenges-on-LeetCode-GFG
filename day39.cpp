4fde863985ceeb7eafae50a4c7b26dea97847077
S.Lakshmanan <d45624312@gmail.com>
2025-04-29 21:22:46 +0530

    Create day39.cpp

---
+++ day39.cpp
@@ -0,0 +1,43 @@
+/* Count Subarrays Where Max Element Appears at Least K Times
+You are given an integer array nums and a positive integer k.
+Return the number of subarrays where the maximum element of nums appears at least k times in that subarray.
+A subarray is a contiguous sequence of elements within an array.
+Example 1:
+Input: nums = [1,3,2,3,3], k = 2
+Output: 6
+Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+Example 2:
+Input: nums = [1,4,2,1], k = 3
+Output: 0
+Explanation: No subarray contains the element 4 at least 3 times.*/
+class Solution {
+    int maxi(vector<int>&nums){
+        int ele=0;
+        for(auto it:nums){
+           ele =max(it,ele);
+        }
+        return ele;
+    }
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+     int ele=0;
+     ele=maxi(nums);
+      int i=0,j=0,cnt=0;long long count=0;
+          while(j<nums.size()){
+            if(nums[j]==ele){
+                cnt++;
+            }
+            while(i<=j&&cnt>=k){
+                count+=nums.size()-j;
+                if(nums[i]==ele){
+                    cnt--;
+                }
+                i++;;
+            }
+            j++;
+          } 
+          return count; 
+
+
+    }
+};
